55aa6ef628bf9cb06af1288abbd4b6458ec8df4e
Gordon M. Celesta <gordo@freeshell.org>
2015-02-23 23:58:22 -0800

    Start of layout script

---
+++ scripts/filelayout.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+BASE_DIR="/home/gordo/projects/artbergmann"
+
+# News
+mkdir -p "$BASE_DIR/news/1980-1989/_posts/images"
+mkdir -p "$BASE_DIR/news/1980-1989/_posts/thumbs"
+mkdir -p "$BASE_DIR/news/1990-1999/_posts/images"
+mkdir -p "$BASE_DIR/news/1990-1999/_posts/thumbs"
+mkdir -p "$BASE_DIR/news/2000-2009/_posts/images"
+mkdir -p "$BASE_DIR/news/2000-2009/_posts/thumbs"
+mkdir -p "$BASE_DIR/news/2010-2019/_posts/images"
+mkdir -p "$BASE_DIR/news/2010-2019/_posts/thumbs"
+mkdir -p "$BASE_DIR/news/current/_posts/images"
+mkdir -p "$BASE_DIR/news/current/_posts/thumbs"
+
+# Concerts
+mkdir -p "$BASE_DIR/concerts/_posts/images"
+mkdir -p "$BASE_DIR/concerts/_posts/thumbs"
+
+# Merchandise
+mkdir -p "$BASE_DIR/merchandise/_posts/images"
+mkdir -p "$BASE_DIR/merchandise/_posts/thumbs"
+
+# Samples
+mkdir -p "$BASE_DIR/samples/_posts/images"
+mkdir -p "$BASE_DIR/samples/_posts/thumbs"
+
+# Gallery
+mkdir -p "$BASE_DIR/gallery/_posts/images"
+mkdir -p "$BASE_DIR/gallery/_posts/thumbs"
+
+# Videos
+mkdir -p "$BASE_DIR/videos/_posts/images"
+mkdir -p "$BASE_DIR/videos/_posts/thumbs"
+
