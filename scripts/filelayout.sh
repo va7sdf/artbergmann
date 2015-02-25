d64006f2b87a7e7dc40a2abd81aabe4793cfe1c0
Gordon M. Celesta <gordo@freeshell.org>
2015-02-25 15:35:43 -0800

    Added album directories - excluding contributed albums

--- scripts/filelayout.sh
+++ scripts/filelayout.sh
@@ -36,3 +36,77 @@ mkdir -p "$BASE_DIR/videos/_posts/thumbs"
 
 # Contact
 mkdir -p "$BASE_DIR/contact"
+
+# Albums
+mkdir -p "$BASE_DIR/albums/shmorgs/shmorgs/lyrics"
+mkdir -p "$BASE_DIR/albums/shmorgs/shmorgs/images"
+mkdir -p "$BASE_DIR/albums/shmorgs/shmorgs/thumbs"
+
+mkdir -p "$BASE_DIR/albums/young_canadians/hawaii/lyrics"
+mkdir -p "$BASE_DIR/albums/young_canadians/hawaii/images"
+mkdir -p "$BASE_DIR/albums/young_canadians/hawaii/thumbs"
+
+mkdir -p "$BASE_DIR/albums/young_canadians/this_is_your_life/lyrics"
+mkdir -p "$BASE_DIR/albums/young_canadians/this_is_your_life/images"
+mkdir -p "$BASE_DIR/albums/young_canadians/this_is_your_life/thumbs"
+
+mkdir -p "$BASE_DIR/albums/young_canadians/joyride_on_the_western_front/lyrics"
+mkdir -p "$BASE_DIR/albums/young_canadians/joyride_on_the_western_front/images"
+mkdir -p "$BASE_DIR/albums/young_canadians/joyride_on_the_western_front/thumbs"
+
+mkdir -p "$BASE_DIR/albums/young_canadians/no_escape/lyrics"
+mkdir -p "$BASE_DIR/albums/young_canadians/no_escape/images"
+mkdir -p "$BASE_DIR/albums/young_canadians/no_escape/thumbs"
+
+mkdir -p "$BASE_DIR/albums/los_popularos/born_free/lyrics"
+mkdir -p "$BASE_DIR/albums/los_popularos/born_free/images"
+mkdir -p "$BASE_DIR/albums/los_popularos/born_free/thumbs"
+
+mkdir -p "$BASE_DIR/albums/los_popularos/working_girls/lyrics"
+mkdir -p "$BASE_DIR/albums/los_popularos/working_girls/images"
+mkdir -p "$BASE_DIR/albums/los_popularos/working_girls/thumbs"
+
+mkdir -p "$BASE_DIR/albums/poisoned/poisoned_mc/lyrics"
+mkdir -p "$BASE_DIR/albums/poisoned/poisoned_mc/images"
+mkdir -p "$BASE_DIR/albums/poisoned/poisoned_mc/thumbs"
+
+mkdir -p "$BASE_DIR/albums/poisoned/poisoned_ep/lyrics"
+mkdir -p "$BASE_DIR/albums/poisoned/poisoned_ep/images"
+mkdir -p "$BASE_DIR/albums/poisoned/poisoned_ep/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/vultura_freeway/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/vultura_freeway/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/vultura_freeway/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/poisoned/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/poisoned/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/poisoned/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/lost_art_bergmann/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/lost_art_bergmann/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/lost_art_bergmann/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/crawl_with_me/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/crawl_with_me/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/crawl_with_me/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/sexual_roulette/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/sexual_roulette/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/sexual_roulette/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/art_bergmann/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/art_bergmann/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/art_bergmann/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/what_fresh_hell_is_this/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/what_fresh_hell_is_this/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/what_fresh_hell_is_this/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/songs_for_the_underclass/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/songs_for_the_underclass/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/songs_for_the_underclass/thumbs"
+
+mkdir -p "$BASE_DIR/albums/art_bergmann/design_flaw/lyrics"
+mkdir -p "$BASE_DIR/albums/art_bergmann/design_flaw/images"
+mkdir -p "$BASE_DIR/albums/art_bergmann/design_flaw/thumbs"
+
